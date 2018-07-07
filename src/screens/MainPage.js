3c141f7c1a4cc70b03ebab54469962e0de1ea312
kawasaki-t <kawasaki_t@tenso.com>
2018-07-07 18:27:07 +0900

    tabにアイコンを追加

--- src/screens/MainPage.js
+++ src/screens/MainPage.js
@@ -1,32 +1,38 @@
+import React from "react";
 import { createBottomTabNavigator } from "react-navigation";
 import ProductListScreen from "./ProductList";
 import ChatListScreen from "./ChatList";
 import ExhibitListScreen from "./ExhibitList";
 import SearchScreen from "./Search";
+import Icon from "react-native-vector-icons/Ionicons";
 
 const Main = createBottomTabNavigator({
   TabItem1: {
     screen: ProductListScreen,
     navigationOptions: {
-      tabBarLabel: "商品一覧"
+      tabBarLabel: "商品一覧",
+      tabBarIcon: <Icon name="ios-home" size={30} />
     }
   },
   TabItem2: {
     screen: ChatListScreen,
     navigationOptions: {
-      tabBarLabel: "取引チャット"
+      tabBarLabel: "取引チャット",
+      tabBarIcon: <Icon name="ios-chatboxes" size={30} />
     }
   },
   TabItem3: {
     screen: ExhibitListScreen,
     navigationOptions: {
-      tabBarLabel: "出品"
+      tabBarLabel: "出品",
+      tabBarIcon: <Icon name="ios-camera" size={30} />
     }
   },
   TabItem4: {
     screen: SearchScreen,
     navigationOptions: {
-      tabBarLabel: "検索"
+      tabBarLabel: "検索",
+      tabBarIcon: <Icon name="ios-search" size={30} />
     }
   }
 });
