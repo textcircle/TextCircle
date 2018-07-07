ed8730425305085cec972db040a058e478c6dd48
kawasaki-t <kawasaki_t@tenso.com>
2018-07-07 15:12:26 +0900

    サイドバーのメニュー項目を追加

--- src/screens/Sidebar.js
+++ src/screens/Sidebar.js
@@ -1,11 +1,31 @@
 import { createDrawerNavigator } from "react-navigation";
 import MainPage from "./MainPage";
+import InquiryScreen from "./Inquiry";
+import ExhibitList from "./ExhibitList";
 
 const Sidebar = createDrawerNavigator({
   DrawerItem1: {
     screen: MainPage,
     navigationOptions: {
-      drawerLabel: "Item1"
+      drawerLabel: "ホーム"
+    }
+  },
+  DrawerItem2: {
+    screen: ExhibitList,
+    navigationOptions: {
+      drawerLabel: "出品した商品"
+    }
+  },
+  DrawerItem3: {
+    screen: InquiryScreen,
+    navigationOptions: {
+      drawerLabel: "お問い合わせ"
+    }
+  },
+  DrawerItem4: {
+    screen: MainPage,
+    navigationOptions: {
+      drawerLabel: "ログアウト"
     }
   }
 });
