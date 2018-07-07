dbae05c04a6052fb1fb826b429b92acc2786dfb0
kawasaki-t <kawasaki_t@tenso.com>
2018-07-07 19:34:18 +0900

    お問い合わせのレイアウト修正

--- src/screens/Inquiry.js
+++ src/screens/Inquiry.js
@@ -1,17 +1,67 @@
 import React from "react";
-import { Text } from "react-native";
+import {
+  Text,
+  View,
+  Button,
+  StyleSheet,
+  KeyboardAvoidingView
+} from "react-native";
 import { createStackNavigator } from "react-navigation";
 import HeaderButton from "../components/HeaderButton";
+import DefaultInput from "../components/DefaultInput";
 
 class Inquiry extends React.Component {
-  constructor(props) {
-    super(props);
-  }
+  state = {
+    name: "",
+    mail: "",
+    content: ""
+  };
 
   render() {
-    return <Text>test</Text>;
+    return (
+      <KeyboardAvoidingView style={styles.container}>
+        <View style={styles.inputContainer}>
+          <Text style={styles.title}>お問い合わせ</Text>
+          <Text>名前</Text>
+          <DefaultInput
+            onChangeText={name => this.setState({ name })}
+            value={this.state.name}
+          />
+          <Text>メールアドレス</Text>
+          <DefaultInput
+            onChangeText={mail => this.setState({ mail })}
+            value={this.state.mail}
+          />
+          <Text>お問い合わせ内容</Text>
+          <DefaultInput
+            onChangeText={content => this.setState({ content })}
+            value={this.state.content}
+          />
+          <Button
+            style={styles.button}
+            title="問い合わせる"
+            onPress={() => console.log("pressed")}
+          />
+        </View>
+      </KeyboardAvoidingView>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: "center",
+    marginTop: 50
+  },
+  inputContainer: {
+    width: "80%"
+  },
+  title: {
+    fontSize: 26,
+    marginBottom: 30
+  }
+});
 
 export default createStackNavigator(
   {
