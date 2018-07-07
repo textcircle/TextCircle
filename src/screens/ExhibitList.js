962bfed1527b9cff29826d29dda69c0e01e53cc6
kawasaki-t <kawasaki_t@tenso.com>
2018-07-07 15:47:10 +0900

    出品した商品一覧ページを作成

--- src/screens/ExhibitList.js
+++ src/screens/ExhibitList.js
@@ -1,8 +1,55 @@
 import React from "react";
-import { Text } from "react-native";
+import { Button, View, TouchableOpacity } from "react-native";
+import { createStackNavigator } from "react-navigation";
+import Detail from "./Detail";
+import Chat from "./Chat";
+import Icon from "react-native-vector-icons/Ionicons";
 
-export default class ProductList extends React.Component {
+class ProductList extends React.Component {
+  constructor(props) {
+    super(props);
+  }
+
   render() {
-    return <Text>test</Text>;
+    return (
+      <View>
+        <Icon size={30} name="ios-trash" />
+        <Button
+          onPress={() => this.props.navigation.navigate("DetailScreen")}
+          title="detail"
+        />
+      </View>
+    );
   }
 }
+
+export default createStackNavigator(
+  {
+    ProductListScreen: {
+      screen: ProductList,
+      navigationOptions: ({ navigation }) => ({
+        title: "出品した商品",
+        headerLeft: (
+          <TouchableOpacity
+            onPress={() => {
+              navigation.toggleDrawer();
+            }}
+          >
+            <Icon name="ios-menu" size={30} />
+          </TouchableOpacity>
+        )
+      })
+    },
+    DetailScreen: {
+      screen: Detail,
+      title: "商品詳細"
+    },
+    ChatScreen: {
+      screen: Chat,
+      title: "取引"
+    }
+  },
+  {
+    initialRouteName: "ProductListScreen"
+  }
+);
