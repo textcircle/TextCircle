cca72d13dd0e698fb612de0363d4beb300f50aef
kawasaki-t <kawasaki_t@tenso.com>
2018-07-07 18:33:06 +0900

    マイページの追加

--- src/screens/Sidebar.js
+++ src/screens/Sidebar.js
@@ -2,6 +2,7 @@ import { createDrawerNavigator } from "react-navigation";
 import MainPage from "./MainPage";
 import InquiryScreen from "./Inquiry";
 import ExhibitList from "./ExhibitList";
+import Mypage from "./Mypage";
 
 const Sidebar = createDrawerNavigator({
   DrawerItem1: {
@@ -11,18 +12,24 @@ const Sidebar = createDrawerNavigator({
     }
   },
   DrawerItem2: {
+    screen: Mypage,
+    navigationOptions: {
+      drawerLabel: "マイページ"
+    }
+  },
+  DrawerItem3: {
     screen: ExhibitList,
     navigationOptions: {
       drawerLabel: "出品した商品"
     }
   },
-  DrawerItem3: {
+  DrawerItem4: {
     screen: InquiryScreen,
     navigationOptions: {
       drawerLabel: "お問い合わせ"
     }
   },
-  DrawerItem4: {
+  DrawerItem5: {
     screen: MainPage,
     navigationOptions: {
       drawerLabel: "ログアウト"
